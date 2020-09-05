6aba4bbac9dc1485abe8273f22066fd7f73678dd
Yuya Makiyama <yuya@YuyanoMacBook-Pro.local>
2020-09-05 19:34:26 +0900

    投稿機能を追加

--- html/index.php
+++ html/index.php
@@ -2,7 +2,17 @@
 session_start();
 require('connect.php');
 
+if(isset($_SESSION['id']) && $_SESSION['time'] + 60*60*6 > time()){
+  $_SESSION['time'] = time();
+  $members = $db->prepare('SELECT * FROM members WHERE id=?');
+  $members->execute(array($_SESSION['id']));
+  $member = $members->fetch();
 
+  $_SESSION['user_id'] = $member['user_id'];
+}else{
+  header('Location: join/index.php');
+  exit();
+}
 
 
 ?>
@@ -14,31 +24,16 @@ require('connect.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <title>PHP Practice</title>
+  <title>PHP Practice | ホーム</title>
 </head>
 <body class="bg-light">
   <div class="container">
 
-    <h2><?php ?></h2>
+    <div class="row mt-5">
+      <div class="col-10"><h2><?php echo $member['name']; ?>さん</h2></div>
+      <div class="col-2"><a href="post.php" class="btn btn-primary w-100">投稿する</a></div>
+    </div>
 
-    <div class="row">
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-    </div>
-    <div class="row">
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-    </div>
-    <div class="row">
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-      <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
-    </div>
     <div class="row">
       <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
       <a href="" class="col-md-3 p-0"><img src="" class="w-100"></a>
--- html/post.php
+++ html/post.php
@@ -1,12 +1,65 @@
+<?php
+session_start();
+require('connect.php');
+
+if(!empty($_POST)){
+  $filename = $_FILES['picture']['name'];
+
+  if(empty($filename)){
+    $error['picture'] = 'blank';
+  }else{
+    $ext = substr($filename, -3);
+    if($ext != 'jpg' && $ext != 'gif' && $ext != 'png'){
+      $error['picture'] = 'type';
+    }
+  }
+
+  if(empty($error)){
+    $image = date('YmdHis') . $__FILES['picture']['name'];
+    move_uploaded_file($_FILES['picture']['tmp_name'], 'img/'. $image);
+    $post = $db->prepare('INSERT INTO posts SET user_id=?, picture=?, comment=?, created=NOW()');
+    $post->execute(array(
+      $_SESSION['user_id'],
+      $image,
+      $_POST['comment']
+    ));
+
+    unset($_SESSION['user_id']);
+
+    header('Location: index.php');
+    exit();
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <title>PHP Practice</title>
+  <title>PHP Practice | 投稿</title>
 </head>
 <body>
-
+  <div class="container mt-5 w-25">
+    <form action="" method="post" enctype="multipart/form-data">
+      <div class="mb-5">
+        <p class="text-muted">投稿する写真を選択してください</p>
+        <div class="text-center"><input type="file" name="picture"></div>
+        <?php if($error['picture'] === 'blank'): ?>
+          <p class="text-danger">※写真を選択してください</p>
+        <?php elseif($error['picture'] === 'type'): ?>
+          <p class="text-danger">※投稿できるのは拡張子が '.gif' , '.jpg' , '.png' の写真のみです</p>
+        <?php endif; ?>
+      </div>
+      <div class="mb-5">
+        <p class="text-muted">コメント入力してください</p>
+        <textarea name="comment" rows="5" class="w-100"><?php echo htmlspecialchars($_POST['comment'], ENT_QUOTES); ?></textarea>
+      </div>
+      <div class="text-center mt-4"><input type="submit" value="投稿" class="btn btn-primary w-100"></div>
+    </form>
+  </div>
 </body>
 </html>
